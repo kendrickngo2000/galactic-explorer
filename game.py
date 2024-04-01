dfb2759841eb6153592e74b9705bed6458dfa144
Kendrick Ngo <kendrickngo2000@aim.com>
2024-03-31 20:45:05 -0700

    added movement keys

--- game.py
+++ game.py
@@ -2,16 +2,38 @@
 import sys
 import pygame
 
-pygame.init()
-pygame.display.set_caption('galactic explorer')
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self): 
+        pygame.init()
+        pygame.display.set_caption('galactic explorer')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        
-    pygame.display.update()
-    clock.tick(60)
+        self.img_pos = [160, 260]
+        self.movement = [False, False] 
+
+    def run(self): 
+        while True:
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+                             
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False   
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False  
+                         
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run() 
